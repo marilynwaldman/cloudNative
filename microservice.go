8956fab811328eff50d358fd736348ebf13ce72c
MarilynWaldman <mdwaldman22@hotmail.com>
2017-12-05 13:09:31 -0700

    full rest api - haven't tried docker yet

--- microservice.go
+++ microservice.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-"fmt"
-"net/http"
-"os"
+	"fmt"
+	"net/http"
+	"os"
 
-
+	"github.com/user/cloudNative/api"
 )
 
 func main() {
-http.HandleFunc("/", index)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/echo", api.EchoHandleFunc)
+	http.HandleFunc("/api/hello", api.HelloHandleFunc)
 
-http.ListenAndServe(port(), nil)
+	http.HandleFunc("/api/books", api.BooksHandleFunc)
+	http.HandleFunc("/api/books/", api.BookHandleFunc)
+
+	http.ListenAndServe(port(), nil)
 }
 
 func port() string {
-port := os.Getenv("PORT")
-if len(port) == 0 {
-port = "8080"
-}
-return ":" + port
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-w.WriteHeader(http.StatusOK)
-fmt.Fprintf(w, "Welcome to Cloud Native Go.")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Welcome to Cloud Native Go.")
 }
+
